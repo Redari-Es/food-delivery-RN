fd5ad79120caf23dd89d3ad684f93240b84cdab0
Redari-Es <905422030@qq.com>
2024-04-27 03:23:11 +0800

    close log

--- client/screens/HomeScreen.jsx
+++ client/screens/HomeScreen.jsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
 
  useEffect(()=>{
   getFeaturedRestaurants().then(data=>{
-    console.log('got data:',data)
+    // console.log('got data:',data)
     setFeaturedCategories(data)
   })
  }) 
